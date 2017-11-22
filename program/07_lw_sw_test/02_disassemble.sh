b575199abb885d45df215f026b47180ec5db1ef3
Oleg Lyovin <olegartys@gmail.com>
2017-11-23 00:05:57 +0300

    program/lw_sw_test: add test for lw/sw instructions
    Signed-off-by: Oleg Lyovin <olegartys@gmail.com>

---
+++ program/07_lw_sw_test/02_disassemble.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+<<<<<<< Updated upstream
+mips-mti-elf-objdump -M no-aliases -D program.elf > program.dis
+=======
+mips-mti-elf-objdump -M "no-aliases" -D program.elf > program.dis
+>>>>>>> Stashed changes
